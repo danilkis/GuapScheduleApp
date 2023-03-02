57040e0b9e8b349b1c498011e2de0a5499c3fba3
triks000 <noybri2013@gmail.com>
2023-03-02 22:04:25 +0300

    toggleButtonWeek

--- app/src/main/java/com/example/guap31/MainActivity.kt
+++ app/src/main/java/com/example/guap31/MainActivity.kt
@@ -1,27 +1,21 @@
 package com.example.guap31
 
 import android.annotation.SuppressLint
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
-import android.view.View
-import android.widget.AdapterView
 import android.widget.ArrayAdapter
 import android.widget.AutoCompleteTextView
-import android.widget.Toast
-
+import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.RecyclerView
 import com.example.guap31.SH_day_adapter.SH_day_adapter
 import com.example.guap31.SH_day_adapter.day_info
 import com.example.guap31.SH_lesson_adapter.Lesson_info
 import com.example.guap31.databinding.ActivityMainBinding
+import com.google.android.material.button.MaterialButtonToggleGroup
 import org.json.JSONObject
 
-class MainActivity : AppCompatActivity() {
 
-    // Подключение Binding ActivityMain
-    private lateinit var mainBinding: ActivityMainBinding
-    /*------------------------------------------------------------*/
+class MainActivity : AppCompatActivity() {
 
     private lateinit var groupLink: MutableMap<String, String>
     private lateinit var lessonTime: JSONObject
@@ -38,7 +32,14 @@ class MainActivity : AppCompatActivity() {
     private lateinit var adapterGroup: ArrayAdapter<String>
     /*------------------------------------------------------------*/
 
-    var item: String = "0"
+    // Параметры для формирования recyclerView с расписанием
+    private var groupItem: String = "0" // Номер групп
+    private var weekItem: String = "1" // Тип нидели(Числитель || Знаменатель)
+    /*------------------------------------------------------------*/
+
+    // Подключение Binding ActivityMain
+    private lateinit var mainBinding: ActivityMainBinding
+    /*------------------------------------------------------------*/
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -48,8 +49,26 @@ class MainActivity : AppCompatActivity() {
         cinnectAllJsonFile()
 
         init_AutoCompleteTextGroup(ArrayList<String>(groupLink.keys))
+        init_toggleButtonWeek()
 
         init_day_list()
+    }
+
+    // Инцелизация toggleButtonWeek menu для нидели
+    private fun init_toggleButtonWeek() {
+        mainBinding.toggleButtonWeek.addOnButtonCheckedListener(
+            MaterialButtonToggleGroup.OnButtonCheckedListener { group, checkedId, isChecked ->
+                if (isChecked) {
+                    weekItem = when (checkedId) {
+                        mainBinding.buttonDenominator.id -> "1"
+                        mainBinding.buttonToday.id -> "1"
+                        mainBinding.buttonNumerator.id -> "2"
+                        else -> "2"
+                    }
+
+                    init_day_list()
+                }
+            })
     }
 
     // Сборка подключение всех JSON Файлов
@@ -66,11 +85,10 @@ class MainActivity : AppCompatActivity() {
         autoComplecteGroup.setAdapter(adapterGroup)
 
         autoComplecteGroup.setOnItemClickListener { parent, view, position, id ->
-            item = groupLink[parent.getItemAtPosition(position)].toString()
+            groupItem = groupLink[parent.getItemAtPosition(position)].toString()
             init_day_list()
         }
     }
-
 
     private fun init_day_list() {
         recyclerView = mainBinding.RViewDay
@@ -90,10 +108,7 @@ class MainActivity : AppCompatActivity() {
         val outputJsDateString = getShJson("date")
         for (i in nameDayList.keys) {
             val LessonList = ArrayList<Lesson_info>()
-
-            val lessons = getShLesson(
-                outputJsDateString, item,
-                "1", i)
+            val lessons = getShLesson(outputJsDateString, groupItem, weekItem, i)
 
             lessons.keys().forEach { keyLes ->
                 if (lessons[keyLes].toString() != "")
@@ -110,7 +125,6 @@ class MainActivity : AppCompatActivity() {
 
         adapter.setScheduleList(DayList)
     }
-
 
     private fun getLinkGroup(nameJsonFile: String): MutableMap<String, String> {
         val LinkGroup = getShJson(nameJsonFile)
